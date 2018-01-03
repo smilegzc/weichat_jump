31d893496a87cc3b70a194d4f4c297e9097d189e
smilegzc <34328745+smilegzc@users.noreply.github.com>
2018-01-03 10:07:44 +0800

    Add files via upload

---
+++ src/AdbCaller.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AdbCaller {
+
+	private static String adbPath = Constants.ADB_PATH;
+
+	private static String screenshotLocation = Constants.SCREENSHOT_LOCATION;
+
+	public static void setAdbPath(String adbPath) {
+		AdbCaller.adbPath = adbPath;
+	}
+
+	public static void setScreenshotLocation(String screenshotLocation) {
+		AdbCaller.screenshotLocation = screenshotLocation;
+	}
+
+	/**
+	 * 调用adb长按屏幕
+	 * 
+	 * @param timeMilli
+	 */
+	public static void longPress(double timeMilli) {
+		try {
+			Process process = Runtime.getRuntime()
+					.exec(adbPath + " shell input touchscreen swipe 170 187 170 187 " + (int) timeMilli);
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+			String s;
+			while ((s = bufferedReader.readLine()) != null)
+				System.out.println(s);
+			process.waitFor();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 改进的截图方法<br>
+	 * 感谢 hxzqlh
+	 */
+	public static void printScreen() {
+
+		try {
+			String[] args = new String[] { "bash", "-c", adbPath + " exec-out screencap -p > " + screenshotLocation };
+			String os = System.getProperty("os.name");
+			if (os.toLowerCase().startsWith("win")) {
+				args[0] = "cmd";
+				args[1] = "/c";
+			}
+			Process p1 = Runtime.getRuntime().exec(args);
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(p1.getErrorStream()));
+			String s;
+			while ((s = bufferedReader.readLine()) != null)
+				System.out.println(s);
+			p1.waitFor();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/Constants.java
@@ -0,0 +1,44 @@
+public class Constants {
+    /**
+     * adb所在位置
+     */
+    public static final String ADB_PATH = "E:\\adb\\adb.exe";
+    /**
+     * 截屏文件所在位置
+     */
+    public static final String SCREENSHOT_LOCATION = "C:\\Users\\RoyZ\\Desktop\\untitled\\s.png";
+
+    /**
+     * 窗体显示的图片宽度
+     */
+    public static final int RESIZED_SCREEN_WIDTH = 675;
+
+    /**
+     * 窗体显示的图片高度
+     */
+    public static final int RESIZED_SCREEN_HEIGHT = 1200;
+
+    /**
+     * 在675*1200分辨率下，跳跃蓄力时间与距离像素的比率<br>
+     * 可根据实际情况自行调整
+     */
+    public static final float RESIZED_DISTANCE_PRESS_TIME_RATIO = 2.175f;
+
+    /**
+     * 截图间隔
+     */
+    public static final int SCREENSHOT_INTERVAL = 3000; // ms
+
+    /**
+     * 手动模式
+     */
+    public static final int MODE_MANUAL = 1;
+    /**
+     * 半自动模式,只需要点secondPoint
+     */
+    public static final int MODE_SEMI_AUTO = 2;
+    /**
+     * 自动模式
+     */
+    public static final int MODE_AUTO = 3;
+}
